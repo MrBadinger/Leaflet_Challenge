e9f520a2a16ed57c4a7d07e2005d91481c545074
Michael Badinger <mcbadinger@gmail.com>
2021-01-30 20:09:29 -0600

    attempt at adding markers

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -84,7 +84,7 @@ var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geo
 var map = L.map("map", {
     center: [37.655961, -122.055910],
     zoom: 7,
-    });
+});
 
 d3.json(url, function(data) {
     event_data = data.features;
@@ -94,7 +94,7 @@ d3.json(url, function(data) {
 
 function createMap(event_data) {
 
-    // Create the tile layer that will be the background of our map
+    // Code to create a clean map
     var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
       maxZoom: 18,
@@ -102,6 +102,68 @@ function createMap(event_data) {
       accessToken: API_KEY
     });
   
-   
     darkmap.addTo(map);
+
+
+
+
+    // set marker radius based on magnitude
+    function quakeRadius(magnitude) {
+        radius = ((magnitude === 0) ? (1) : magnitude * 5)
+        return radius;
+      }
+  
+      // function returns the stlye for the markers
+      function getStyle(feature) {
+  
+        markerStyle = {
+          opacity: 0.75,
+          fillOpacity: 1,      
+          color: "black",
+          stroke: true,
+          weight: 0.75,
+          // set fillcolor by passing quake depth to getColor
+          fillColor: getColor(feature.geometry.coordinates[2]),
+          // set radius by passing quake magnitude to quakeRadius
+          radius: quakeRadius(feature.properties.mag),
+  
+        };
+        return markerStyle
+      }
+      
+      // getColor function returns colors based on quake depth
+        function getColor(depth) {
+  
+          var color = ((depth >= 100) ? ("#EE0000") : ((depth >= 80) ? ("#FF6333") : ((depth >= 60) ? ("#FFA500") : ((depth >= 40) ? ("#FFCC11"):  ((depth >= 20) ? ("#FFEE00"):"#ABCD00")))))
+        
+          return color;
+        }
+
+        event_layer = L.geoJson(event_data, {
+
+            pointToLayer: function(feature, location) {
+              return L.circleMarker(location);
+            },
+            style: getStyle,
+    
+            onEachFeature: function(feature, layer) {
+              layer.bindPopup("<h3>" + earthquake.properties.type +
+              "<h3><h3>Location: " + earthquake.properties.place +
+              "<h3><h3>Magnitude: " + earthquake.properties.mag + 
+              "<h3><h3>Longitude: " + earthquake.geometry.coordinates[0] + 
+              "<h3><h3>Latitude: " + earthquake.geometry.coordinates[1] + 
+              "<h3><h3>Depth (km): " + earthquake.geometry.coordinates[2] + 
+              "</h3>");
+            }
+    
+          });
+          
+          event_layer.addTo(myMap);
+
+
+
+
+
+
 }
+
